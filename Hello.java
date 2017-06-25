6c8e60b687ac0fd1b92e9f13e236277d85b2ca97
andy <815846025@qq.com>
2017-06-25 13:05:51 +0800

    simple print

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
  public class Hello {
-public static final String COMPANY_NAME="QIRUI".
+
 public static void main(String[]args){
-System.out.println("Happy New Year");
-System.out.println("Happy Myself");
+
+System.out.println("=================");
 System.out.println("****************");
  }
 
