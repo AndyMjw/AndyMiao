d5ebcf9f3c7047e57d243ecbb274cf32051e14cd
andy <815846025@qq.com>
2017-06-25 13:08:26 +0800

    hello print

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@
 
 public static void main(String[]args){
 
-System.out.println("=================");
-System.out.println("****************");
+System.out.println("Hello World");
+System.out.println("Hello Andy");
  }
 
 }
