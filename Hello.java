39f0cfe7fd08c190b1ced0b5edc8d735ccbf4bb1
andy <815846025@qq.com>
2017-06-25 13:01:01 +0800

    add static attribute

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
  public class Hello {
-
+public static final String COMPANY_NAME="QIRUI".
 public static void main(String[]args){
 System.out.println("Happy New Year");
 System.out.println("Happy Myself");
